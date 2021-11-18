0877e819cd09fb31ac7ee872d8724148bb5c1be9
Timur Tül <38362206+timurtul@users.noreply.github.com>
2021-11-18 16:59:55 +0300

    Create methodlar.cs

---
+++ methodlar.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodornekler
+{
+    class methodlar
+    {
+
+        public int Toplama  (int sayi1, int sayi2)
+        {
+
+            int Sonuc = sayi1 + sayi2;
+            return Sonuc;
+
+        }
+               
+    
+    }
+}
